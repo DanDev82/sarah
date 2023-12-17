ebad67e56028d61adacd4838de408bac6780dfba
Dan Dev <ifdanthencool@gmail.com>
2023-12-17 12:31:04 -0600

    Updated links

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoOpenOutline } from "react-icons/io5";
+import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa"
 /***** 
  * Light Blue #C7E9F1
  * Lemon Chiffon #FEF9C8
@@ -16,23 +17,24 @@ function Cards() {
         <h2 className="text-4xl text-center mb-10 font-bold">My Links</h2>
         <div className="card-row ">
           <div className="card bg-[#C7E9F1]">
-            <a href="#">
-              <span className="mr-4">Card 1</span>
-              <IoOpenOutline  className="text-2xl"/>  
+            
+            <a href="https://www.facebook.com/profile.php?id=61553238343927">
+              <span className="mr-4"><FaFacebookF className="text-4xl"/></span>
+              {/* <IoOpenOutline  className="text-2xl"/>   */}
             </a>
             
           </div>
           <div className="card bg-[#FEF9C8]">
-            <a href="#">
-              <span className="mr-4">Card 2</span>
-              <IoOpenOutline  className="text-2xl"/>  
+            <a href="https://www.instagram.com/magicalmombossaffiliate/">
+              <span className="mr-4"><FaInstagram className="text-4xl"/></span>
+              {/* <IoOpenOutline  className="text-2xl"/>   */}
             </a>
             
           </div>
           <div className="card bg-[#F0CDFD]">
-            <a href="#">
-              <span className="mr-4">Card 3</span>
-              <IoOpenOutline  className="text-2xl"/>  
+            <a href="https://www.tiktok.com/@magicalmombossaffiliate">
+              <span className="mr-4"><FaTiktok className="text-4xl"/></span>
+              {/* <IoOpenOutline  className="text-2xl"/>   */}
             </a>
             
           </div>
